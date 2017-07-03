a24a0718b891a0c365d1de788db7037ccc0a7d50
Christian Tietze <christian.tietze@gmail.com>
2017-07-03 10:19:04 +0200

    Initial Commit

---
+++ Typewriter/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  Typewriter
+//
+//  Created by Christian Tietze on 03.07.17.
+//  Copyright © 2017 Christian Tietze. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ Typewriter/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  Typewriter
+//
+//  Created by Christian Tietze on 03.07.17.
+//  Copyright © 2017 Christian Tietze. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
