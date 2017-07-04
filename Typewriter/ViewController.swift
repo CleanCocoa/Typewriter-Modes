48606e27ed068667239d8b2b854ba50687dbdf52
Christian Tietze <christian.tietze@gmail.com>
2017-07-04 08:42:16 +0200

    enqueue typing changes to scroll after layout

--- Typewriter/ViewController.swift
+++ Typewriter/ViewController.swift
@@ -8,7 +8,7 @@
 
 import Cocoa
 
-class ViewController: NSViewController, NSTextViewDelegate {
+class ViewController: NSViewController, NSTextViewDelegate, NSLayoutManagerDelegate {
 
     @IBOutlet weak var scrollView: NSScrollView!
     @IBOutlet weak var clipView: NSClipView!
@@ -18,41 +18,34 @@ class ViewController: NSViewController, NSTextViewDelegate {
         super.viewDidLoad()
         textView.string = try! String(contentsOf: URL(fileURLWithPath: "/Users/ctm/Archiv/§ O reswift.md"))
         textView.textContainerInset = NSSize(width: 0, height: scrollView.bounds.height / 2)
+        textView.layoutManager?.delegate = self
     }
+
+    func layoutManager(_ layoutManager: NSLayoutManager, didCompleteLayoutFor textContainer: NSTextContainer?, atEnd layoutFinishedFlag: Bool) {
+
+        guard let textContainer = textContainer else { return }
+        guard let characterRange = self.pendingTypingChangeRange else { return }
+        self.pendingTypingChangeRange = nil
+
+        let activeRange = layoutManager.glyphRange(forCharacterRange: characterRange, actualCharacterRange: nil)
+        let rect = layoutManager.boundingRect(forGlyphRange: activeRange, in: textContainer)
+        let bottom = rect.origin
+        textView.scroll(bottom)
+    }
+
+    var pendingTypingChangeRange: NSRange?
 
     func textView(_ textView: NSTextView, shouldChangeTextIn affectedCharRange: NSRange, replacementString: String?) -> Bool {
 
         guard let text = replacementString else { return true }
 
-        let rect = self.boundingRect(selectedRange: affectedCharRange, text: text)
-        let bottom = rect.origin
-//            .applying(CGAffineTransform(translationX: 0, y: -halfScreen)) // not needed with container insets?
-        textView.scroll(bottom)
+        let expectedLength = text.isEmpty ? 0 : affectedCharRange.length + text.nsLength
+        pendingTypingChangeRange = NSRange(
+            location: affectedCharRange.location + expectedLength,
+            length: 0)
+
         return true
     }
-
-    func boundingRect(selectedRange range: NSRange, text: String) -> NSRect {
-
-        guard
-            let baseTextContainer = textView.textContainer,
-            let baseTextStorage = textView.textStorage
-            else { preconditionFailure() }
-
-        let layoutManager = NSLayoutManager()
-        let textContainer = NSTextContainer(containerSize: baseTextContainer.containerSize)
-        layoutManager.addTextContainer(textContainer)
-        defer { layoutManager.removeTextContainer(at: 0) }
-        let textStorage = NSTextStorage(attributedString: baseTextStorage)
-        textStorage.addLayoutManager(layoutManager)
-        defer { textStorage.removeLayoutManager(layoutManager) }
-
-        textStorage.replaceCharacters(in: range, with: text)
-        let length = text.isEmpty ? 0 : range.length + text.nsLength
-        let changeRange = NSRange(location: range.location + length, length: 0)
-        let activeRange = layoutManager.glyphRange(forCharacterRange: changeRange, actualCharacterRange: nil)
-        return layoutManager.boundingRect(forGlyphRange: activeRange, in: textContainer)
-    }
-
 }
 
 extension String {
