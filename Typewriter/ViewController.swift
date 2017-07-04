b81b7fe2d8a815e04b69d043570c5c9084d420c5
Christian Tietze <christian.tietze@gmail.com>
2017-07-04 11:08:40 +0200

    fix scrolling (except for space)

--- Typewriter/ViewController.swift
+++ Typewriter/ViewController.swift
@@ -8,7 +8,7 @@
 
 import Cocoa
 
-class ViewController: NSViewController, NSTextViewDelegate, NSLayoutManagerDelegate {
+class ViewController: NSViewController, NSTextStorageDelegate {
 
     @IBOutlet weak var scrollView: NSScrollView!
     @IBOutlet weak var clipView: NSClipView!
@@ -18,50 +18,19 @@ class ViewController: NSViewController, NSTextViewDelegate, NSLayoutManagerDeleg
         super.viewDidLoad()
         textView.string = try! String(contentsOf: URL(fileURLWithPath: "/Users/ctm/Archiv/§ O reswift.md"))
         textView.textContainerInset = NSSize(width: 0, height: scrollView.bounds.height / 2)
-        textView.layoutManager?.delegate = self
+        textView.textStorage?.delegate = self
     }
 
-    func layoutManager(_ layoutManager: NSLayoutManager, didCompleteLayoutFor textContainer: NSTextContainer?, atEnd layoutFinishedFlag: Bool) {
+    func textStorage(_ textStorage: NSTextStorage, didProcessEditing editedMask: NSTextStorageEditActions, range editedRange: NSRange, changeInLength delta: Int) {
 
-        guard let textContainer = textContainer else { return }
-        guard let characterRange = self.pendingTypingChangeRange else { return }
-        self.pendingTypingChangeRange = nil
+        guard let window = textView.window else { return }
+        var ptr = NSRange.init()
+        let visibleRect = textView.firstRect(forCharacterRange: editedRange, actualRange: &ptr)
+        let editedRect = textView.convert(window.convertFromScreen(visibleRect), from: nil)
 
-        let activeRange = layoutManager.glyphRange(forCharacterRange: characterRange, actualCharacterRange: nil)
-        let rect = layoutManager.boundingRect(forGlyphRange: activeRange, in: textContainer)
-        let bottom = rect.origin
+        let bottom = editedRect.origin
+            .applying(.init(translationX: 0, y: scrollView.bounds.height / 2))
         textView.scroll(bottom)
-    }
-
-    var pendingTypingChangeRange: NSRange?
-
-    func textView(_ textView: NSTextView, shouldChangeTextIn affectedCharRange: NSRange, replacementString: String?) -> Bool {
-
-        guard let text = replacementString else { return true }
-
-        let expectedLength = text.isEmpty ? 0 : affectedCharRange.length + text.nsLength
-        pendingTypingChangeRange = NSRange(
-            location: affectedCharRange.location + expectedLength,
-            length: 0)
-
-        return true
-    }
-}
-
-extension String {
-    var nsLength: Int {
-        return (self as NSString).length
-    }
-}
-
-extension NSTextView {
-
-    var lineHeight: CGFloat {
-        guard let font = self.font,
-            let layoutManager = self.layoutManager
-            else { return 0 }
-
-        return layoutManager.defaultLineHeight(for: font)
     }
 }
 
